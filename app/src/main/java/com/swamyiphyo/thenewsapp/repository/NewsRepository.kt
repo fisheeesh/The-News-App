336a06cd447efa7ec430b467c9a55c3a57962633
fisheeesh <swanphyo444@gmail.com>
2024-08-12 15:03:49 +0700

    Repository is ready

---
+++ app/src/main/java/com/swamyiphyo/thenewsapp/repository/NewsRepository.kt
@@ -0,0 +1,28 @@
+package com.swamyiphyo.thenewsapp.repository
+
+import com.swamyiphyo.thenewsapp.api.RetrofitInstance
+import com.swamyiphyo.thenewsapp.database.ArticleDatabase
+import com.swamyiphyo.thenewsapp.models.Article
+
+/**
+ * A place or container where sth is deposited or stored
+ * with the help of articleDatabase and retrofitInstance or ApiClient
+ */
+class NewsRepository(private val db : ArticleDatabase) {
+    suspend fun getHeadLines(counterCode : String, pageNumber : Int) {
+        RetrofitInstance.api.getHeadLines(counterCode, pageNumber)
+//        ApiClient.apiService.getHeadLines(counterCode, pageNumber)
+    }
+
+    suspend fun searchNews(searchQuery : String, pageNumber : Int) {
+        RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+//        ApiClient.apiService.searchForNews(searchQuery, pageNumber)
+    }
+
+    suspend fun upsert(article : Article) = db.getArticleDao().upsert(article)
+
+    suspend fun deleteArticle(article: Article) = db.getArticleDao().deleteArticle(article)
+
+    fun getFavoriteNews() = db.getArticleDao().getAllArticles()
+
+}
