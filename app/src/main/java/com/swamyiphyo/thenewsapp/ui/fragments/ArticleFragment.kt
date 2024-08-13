5486d88f78349eee049400891ab1651df497d47a
fisheeesh <swanphyo444@gmail.com>
2024-08-13 21:28:52 +0700

    Article Fragment is ready

--- app/src/main/java/com/swamyiphyo/thenewsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/swamyiphyo/thenewsapp/ui/fragments/ArticleFragment.kt
@@ -2,17 +2,38 @@ package com.swamyiphyo.thenewsapp.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
 import com.swamyiphyo.thenewsapp.R
-class ArticleFragment : Fragment() {
+import com.swamyiphyo.thenewsapp.databinding.FragmentArticleBinding
+import com.swamyiphyo.thenewsapp.ui.NewsActivity
+import com.swamyiphyo.thenewsapp.viewmodel.NewsViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article, container, false)
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+
+    //All API responses and everything is connected in viewmodel
+    lateinit var newsViewModel : NewsViewModel
+    val args : ArticleFragmentArgs by navArgs()
+    private lateinit var binding : FragmentArticleBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentArticleBinding.bind(view)
+
+        newsViewModel = (activity as NewsActivity).newsViewModel
+        val article = args.article
+
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(article.url)
+        }
+
+        binding.fabFavorite.setOnClickListener(){
+            newsViewModel.addToFavorites(article)
+            Snackbar.make(view, "Added to Favorites!", Snackbar.LENGTH_SHORT).show()
+        }
+
     }
 }
