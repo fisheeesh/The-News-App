80884dd8f760954f758cabfa16abccaedea50417
fisheeesh <swanphyo444@gmail.com>
2024-08-14 23:16:24 +0700

    HeadLines Fragment is ready

--- app/src/main/java/com/swamyiphyo/thenewsapp/ui/fragments/HeadLinesFragment.kt
+++ app/src/main/java/com/swamyiphyo/thenewsapp/ui/fragments/HeadLinesFragment.kt
@@ -1,34 +1,95 @@
 package com.swamyiphyo.thenewsapp.ui.fragments
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.swamyiphyo.thenewsapp.R
 import com.swamyiphyo.thenewsapp.adapters.NewsAdapter
 import com.swamyiphyo.thenewsapp.databinding.FragmentHeadLinesBinding
 import com.swamyiphyo.thenewsapp.databinding.FragmentSearchBinding
+import com.swamyiphyo.thenewsapp.ui.NewsActivity
 import com.swamyiphyo.thenewsapp.util.Constants
+import com.swamyiphyo.thenewsapp.util.Resource
 import com.swamyiphyo.thenewsapp.viewmodel.NewsViewModel
 
+@Suppress("NAME_SHADOWING")
 class HeadLinesFragment : Fragment(R.layout.fragment_head_lines) {
 
-    lateinit var newsViewModel: NewsViewModel
-    lateinit var newsAdapter: NewsAdapter
-    lateinit var errorText : TextView
-    lateinit var retryBtn : Button
-    lateinit var itemHeadLinesError : CardView
+    private lateinit var newsViewModel: NewsViewModel
+    private lateinit var newsAdapter: NewsAdapter
+    private lateinit var errorText : TextView
+    private lateinit var retryBtn : Button
+    private lateinit var itemHeadLinesError : CardView
     private lateinit var binding : FragmentHeadLinesBinding
 
+    @SuppressLint("InflateParams")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHeadLinesBinding.bind(view)
+
+        itemHeadLinesError = view.findViewById(R.id.itemHeadlinesError)
+
+        val inflater = requireContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view : View = inflater.inflate(R.layout.items_error, null)
+
+        retryBtn = view.findViewById(R.id.button_retry)
+        errorText = view.findViewById(R.id.textView_error)
+
+        newsViewModel = (activity as NewsActivity).newsViewModel
+        setUpHeadLinesRecycler()
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+
+            findNavController().navigate(R.id.action_headLinesFragment_to_articleFragment, bundle)
+        }
+
+        newsViewModel.headLines.observe(viewLifecycleOwner, Observer { response ->
+            when(response){
+                is Resource.Success<*> ->{
+                    hideProgressBar()
+                    hideErrorMessage()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles.toList())
+                        val totalPages = newsResponse.totalResults / Constants.QUERY_PAGE_SIZE + 2
+                        isLastPage = newsViewModel.headLinesPage == totalPages
+                        if(isLastPage){
+                            binding.headlinesRV.setPadding(0,0,0,0)
+                        }
+                    }
+                }
+                is Resource.Error<*> ->{
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Toast.makeText(activity, "Sorry Error!", Toast.LENGTH_SHORT).show()
+                        showErrorMessage(message)
+                    }
+                }
+                is Resource.Loading<*> ->{
+                    showProgressBar()
+                }
+            }
+        })
+
+        retryBtn.setOnClickListener(){
+            newsViewModel.getHeadLines("mm")
+        }
+
     }
 
     var isError = false
@@ -82,6 +143,18 @@ class HeadLinesFragment : Fragment(R.layout.fragment_head_lines) {
 
         override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
             super.onScrollStateChanged(recyclerView, newState)
+
+            if(newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                isScrolling = true
+            }
+        }
+    }
+    private fun setUpHeadLinesRecycler(){
+        newsAdapter = NewsAdapter()
+        binding.headlinesRV.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+            addOnScrollListener(this@HeadLinesFragment.scrollListener)
         }
     }
 }
